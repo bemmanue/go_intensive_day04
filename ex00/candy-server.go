bcdae57126bdaba79dc3e8e2a8d44e066b75a8e8
Burma Emmanuel <bemmanue@pr-n6.kzn.21-school.ru>
2022-11-24 15:31:39 +0300

    first commit

---
+++ ex00/candy-server.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type Data struct {
+	Money      int    `json:"money"`
+	CandyType  string `json:"candyType"`
+	CandyCount int    `json:"candyCount"`
+}
+
+func getPrice(candyType string) (int, error) {
+	switch candyType {
+	case "CE":
+		return 10, nil
+	case "AA":
+		return 15, nil
+	case "NT":
+		return 17, nil
+	case "DE":
+		return 21, nil
+	case "YR":
+		return 23, nil
+	default:
+		return 0, errors.New("wrong candy type")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var data Data
+
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		var response interface{}
+		candyPrice, err := getPrice(data.CandyType)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			response = struct {
+				Error string
+			}{"Wrong candy type!"}
+		} else if data.CandyCount < 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			response = struct {
+				Error string
+			}{"Negative candy count!"}
+		} else if candyPrice*data.CandyCount > data.Money {
+			amount := candyPrice*data.CandyCount - data.Money
+			response = struct {
+				Error string
+			}{"You need " + strconv.Itoa(amount) + " more money!"}
+			w.WriteHeader(http.StatusPaymentRequired)
+		} else {
+			change := data.Money - candyPrice*data.CandyCount
+			response = struct {
+				Change int
+				Thanks string
+			}{change, "Thank you!"}
+			w.WriteHeader(http.StatusCreated)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(response)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe("server-cert:8888", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ ex01/client/candy-client.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Data struct {
+	Money      int
+	CandyType  string
+	CandyCount int
+}
+
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
+
+func main() {
+	candyType := flag.String("k", "", "two-letter abbreviation for the candy type")
+	candyCount := flag.Int("c", 0, "count of candy to buy")
+	money := flag.Int("m", 0, "amount of money given to machine")
+	flag.Parse()
+
+	if !isFlagPassed("k") || !isFlagPassed("c") || !isFlagPassed("m") {
+		log.Fatalln("Wrong arguments")
+	}
+
+	var data Data
+	data.Money = *money
+	data.CandyCount = *candyCount
+	data.CandyType = *candyType
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalln("Error encoding data: ", err)
+	}
+
+	rootCA, err := ioutil.ReadFile("./server-cert/cert.pem")
+	if err != nil {
+		log.Fatalf("reading cert failed : %v", err)
+	}
+	rootCAPool := x509.NewCertPool()
+	rootCAPool.AppendCertsFromPEM(rootCA)
+
+	client := http.Client{
+		Timeout: 15 * time.Second,
+		Transport: &http.Transport{
+			IdleConnTimeout: 10 * time.Second,
+			TLSClientConfig: &tls.Config{
+				RootCAs: rootCAPool,
+				GetClientCertificate: func(info *tls.CertificateRequestInfo) (*tls.Certificate, error) {
+					c, err := tls.LoadX509KeyPair("./client-cert/cert.pem", "./client-cert/key.pem")
+					if err != nil {
+						log.Printf("Error loading key pair: %v\n", err)
+						return nil, err
+					}
+					return &c, nil
+				},
+			},
+		}}
+
+	req, err := http.NewRequest(http.MethodPost, "https://localhost:8888", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatalln("Error creating request: ", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln("Error sending request: ", err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+}
